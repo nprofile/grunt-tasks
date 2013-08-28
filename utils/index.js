6851b743c55a54fdc21cb5c48905535c538a7667
tauren <tauren@sportzing.com>
2013-08-28 15:46:41 -0700

    Typo

--- utils/index.js
+++ utils/index.js
@@ -2,5 +2,5 @@ module.exports = {
   getModifyTasks: require('./commands'),
   log: require('./log'),
   wro4j: require('./wro4j'),
-  registerTasks: require('.register')
+  registerTasks: require('./register')
 };
