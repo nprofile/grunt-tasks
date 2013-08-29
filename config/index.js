dd1a7b18a5449f7f4c8632df90e7895138c20bb8
tauren <tauren@sportzing.com>
2013-08-28 17:46:25 -0700

    Don't include src.js more than once

--- config/index.js
+++ config/index.js
@@ -4,17 +4,6 @@ var fs = require('fs');
 
 module.exports = function(configDir) {
 
-  // var localDir = __dirname;
-
-  // var shared = _.extend(
-  //   {},
-  //   require('./shared'),
-  //   require('./exec'),
-  //   require('./shell'),
-  //   require('./growl'),
-  //   require('./jshint')
-  // });
-
   // Placehholder for project config object
   var projectConfig = {};
 
@@ -25,6 +14,11 @@ module.exports = function(configDir) {
   _.each(configModules, function(name) {
     // Make sure file is a javascript file
     if ('.js' !== path.extname(name)) {
+      return;
+    }
+
+    // Don't include src since it is manually added first
+    if ('src.js' === path.basename(name)) {
       return;
     }
 
@@ -44,32 +38,6 @@ module.exports = function(configDir) {
     // ... import project config
     projectConfig
   );
-
-
-    // // Include the source paths config first so other configs can reference it
-    // require(path.resolve(configDir,'src'),
-
-    // // ... import shared config
-    // require(path.resolve(__dirname,'exec'),
-    // require(path.resolve(__dirname,'shell'),
-    // require(path.resolve(__dirname,'growl'),
-    // require(path.resolve(__dirname,'jshint'),
-
-    // // ... import project config
-    // require(path.resolve(configDir,'templatize'),
-    // require(path.resolve(configDir,'concat'),
-    // require(path.resolve(configDir,'concatSourceURL'),
-    // require(path.resolve(configDir,'min'),
-    // require(path.resolve(configDir,'logless'),
-    // require(path.resolve(configDir,'qunit'),
-    // require(path.resolve(configDir,'lint'),
-    // require(path.resolve(configDir,'copy'),
-    // require(path.resolve(configDir,'watch'),
-    // require(path.resolve(configDir,'less'),
-    // require(path.resolve(configDir,'substitute'),
-    // require(path.resolve(configDir,'recess'),
-    // require(path.resolve(configDir,'unbundle'),
-    // require(path.resolve(configDir,'bundle')
 
 };
 
